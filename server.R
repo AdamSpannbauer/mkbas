186af89d7c1449f070f870bd0b315b73c604403a
Adam Spannbauer <SpannbauerAdam@gmail.com>
2022-08-11 13:33:42 -0400

    start styling leaderboard page

--- server.R
+++ server.R
@@ -2,31 +2,45 @@ library(shiny)
 library(DT)
 
 server <- function(input, output, session) {
-  leaderboard_df <- reactivePoll(
+  google_sheets_df <- reactivePoll(
     intervalMillis = GOOGLE_SHEETS_POLL_SECS * 1000,
     session = session,
     checkFunc = read_sheets_df,
     valueFunc = read_sheets_df
-  )
+  ) # reactivePoll
 
-  output$leaderboard_dt <- DT::renderDataTable({
+  leaderboard_df <- reactive({
     display_cols <- c("name", "major", "time")
     display_col_names <- c("Name", "Major", "Time")
 
-    display_df <- leaderboard_df()
-    fastest_times <- aggregate(seconds ~ name + major, display_df, min)
-
-    fastest_times <- merge(
-      fastest_times,
-      display_df,
-      by = c("name", "major", "seconds")
+    # min time per name + major
+    leaderboard_df <- aggregate(
+      seconds ~ name + major,
+      data = google_sheets_df(),
+      FUN = min
     )
 
-    fastest_times <- fastest_times[order(fastest_times$seconds), ]
+    # Get back demo info and sort
+    leaderboard_df <- merge(
+      leaderboard_df,
+      google_sheets_df(),
+      by = c("name", "major", "seconds")
+    )
+    leaderboard_df <- leaderboard_df[order(leaderboard_df$seconds), ]
 
-    fastest_times <- fastest_times[, display_cols]
-    names(fastest_times) <- display_col_names
-    rownames(fastest_times) <- NULL
-    fastest_times
+    # Pretty up
+    leaderboard_df <- leaderboard_df[, display_cols]
+    names(leaderboard_df) <- display_col_names
+    rownames(leaderboard_df) <- NULL
+
+    leaderboard_df
   })
+
+  output$leaderboard_dt <- DT::renderDataTable(
+    leaderboard_df(),
+    options = list(
+      iDisplayLength = 50,
+      bLengthChange = 0
+    )
+  ) # renderDataTable
 }
--- ui.R
+++ ui.R
@@ -1,13 +1,72 @@
 library(shiny)
+library(shinydashboard)
 library(DT)
 
-ui <- fluidPage(
-  tags$head(
-    tags$link(
-      rel = "stylesheet",
-      type = "text/css",
-      href = "style.css"
+
+ui <- dashboardPage(
+  title = "BAS MK8",
+  skin = "red",
+  dashboardHeader(
+    title = span(
+      "BAS MK8",
+      style = "font-family: goshaLight"
     )
-  ),
-  DT::dataTableOutput("leaderboard_dt")
-)
+  ), # dashboardHeader
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem(
+        text = "Leaderboard",
+        tabName = "leaderboard",
+        icon = icon("flag-checkered")
+      ),
+      menuItem(
+        text = "Analysis",
+        tabName = "analysis",
+        icon = icon("chart-bar")
+      )
+    ) # sidebarMenu
+  ), # dashboardSidebar
+  dashboardBody(
+    tags$head(
+      tags$link(
+        rel = "stylesheet",
+        type = "text/css",
+        href = "style.css"
+      )
+    ),
+    tabItems(
+      tabItem(
+        tabName = "leaderboard",
+        box(
+          background = "black",
+          DT::dataTableOutput("leaderboard_dt")
+        ),
+        box(
+          align = "right",
+          background = "black",
+          fluidRow(
+            column(
+              width = 6,
+              span(
+                "Business Analytics Society",
+                class = "maintitle"
+              )
+            ),
+            column(
+              width = 6,
+              img(
+                # style = "position: relative",
+                width = "100%",
+                src = "mkspin.gif"
+              )
+            )
+          )
+        )
+      ),
+      tabItem(
+        tabName = "analysis",
+        h2("Coming soon...")
+      )
+    ) # tabItems
+  ) # dashboardBody
+) # dashboardPage
